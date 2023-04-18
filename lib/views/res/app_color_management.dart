f5cd09b792d9b075799d3704217d07d45df4c8ca
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-04-18 13:52:15 +0430

    Create app_color_management.dart

---
+++ lib/views/res/app_color_management.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColorManagement {
+  static const Color background = Color(0x00ffffff);
+  static const Color onBackground = Color(0x00165c73);
+  static const Color headline = Color(0x00000000);
+  static const Color primary = Color(0x00e49527);
+}
