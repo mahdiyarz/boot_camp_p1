361650b684913a73c3ca4335fb2b8e6751b879cd
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-04-18 13:51:44 +0430

    Create app_image_assets.dart

---
+++ lib/views/res/app_image_assets.dart
@@ -0,0 +1,10 @@
+abstract class AppImageAssets {
+  static const String iconPath = 'assets/icons';
+  static const String menu = '$iconPath/menu.png';
+  static const String notificationBadge = '$iconPath/notification_badge.png';
+  static const String notification = '$iconPath/notification.png';
+  static const String notificationOutline =
+      '$iconPath/notification_outline.png';
+  static const String scan = '$iconPath/scan.png';
+  static const String search = '$iconPath/search.png';
+}
