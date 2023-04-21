7f7f8d3fb292af686fab45331bb37e5720d3cce2
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-04-22 03:37:54 +0430

    Create search_bar.dart

---
+++ lib/views/home/local_widgets/search_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../res/app_assets_management.dart';
+import '../../res/app_color_management.dart';
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 4,
+      ),
+      width: 212,
+      height: 36,
+      decoration: BoxDecoration(
+        color: AppColorManagement.surface,
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Image.asset(AppAssetsManagement.search),
+        ],
+      ),
+    );
+  }
+}
