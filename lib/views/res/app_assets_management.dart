9fa7ab65dbc29048b7b44b6e8e409b402fc99a0c
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-04-22 03:34:28 +0430

    Add library books

--- lib/views/res/app_assets_management.dart
+++ lib/views/res/app_assets_management.dart
@@ -7,4 +7,8 @@ abstract class AppAssetsManagement {
       '$iconPath/notification_outline.png';
   static const String scan = '$iconPath/scan.png';
   static const String search = '$iconPath/search.png';
+
+  static const String bookPath = 'assets/books';
+  static const String libraryBook1 = '$bookPath/library_book_1.png';
+  static const String libraryBook2 = '$bookPath/library_book_2.png';
 }
