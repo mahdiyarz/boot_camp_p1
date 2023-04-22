a85d881d85176e0f6a2fa64dc911887a45442224
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-04-22 17:59:50 +0430

    Create wishlist_tile.dart

---
+++ lib/views/home/local_widgets/wishlist_tile.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+import '../../res/export_resources.dart';
+
+class WishlistTile extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String author;
+  final int star;
+  const WishlistTile({
+    required this.imagePath,
+    required this.title,
+    required this.author,
+    required this.star,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: 60,
+          height: 85,
+          child: Image.asset(imagePath),
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        SizedBox(
+          height: 85,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                title,
+                style: getBoldStyle(
+                  fontColor: AppColorManagement.onBackground,
+                ),
+              ),
+              Text(
+                author,
+                style: getBoldStyle(
+                  fontColor: AppColorManagement.subtitle,
+                ),
+              ),
+              const Expanded(child: SizedBox()),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star_rate_rounded,
+                    size: 10,
+                    color: star >= 1
+                        ? AppColorManagement.primary
+                        : AppColorManagement.disable,
+                  ),
+                  Icon(
+                    Icons.star_rate_rounded,
+                    size: 10,
+                    color: star >= 2
+                        ? AppColorManagement.primary
+                        : AppColorManagement.disable,
+                  ),
+                  Icon(
+                    Icons.star_rate_rounded,
+                    size: 10,
+                    color: star >= 3
+                        ? AppColorManagement.primary
+                        : AppColorManagement.disable,
+                  ),
+                  Icon(
+                    Icons.star_rate_rounded,
+                    size: 10,
+                    color: star >= 4
+                        ? AppColorManagement.primary
+                        : AppColorManagement.disable,
+                  ),
+                  Icon(
+                    Icons.star_rate_rounded,
+                    size: 10,
+                    color: star >= 5
+                        ? AppColorManagement.primary
+                        : AppColorManagement.disable,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
