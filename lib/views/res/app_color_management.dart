57317089594073bb96943652fca8a265d3e117cc
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-04-22 18:00:32 +0430

    Add subtitle rgb color on color management

--- lib/views/res/app_color_management.dart
+++ lib/views/res/app_color_management.dart
@@ -4,6 +4,7 @@ abstract class AppColorManagement {
   static const Color background = Color.fromRGBO(255, 255, 255, 1);
   static const Color onBackground = Color.fromRGBO(22, 92, 115, 1);
   static const Color headline = Color.fromRGBO(0, 0, 0, 1);
+  static const Color subtitle = Color.fromRGBO(151, 151, 151, 1);
   static const Color primary = Color.fromRGBO(228, 149, 39, 1);
   static const Color surface = Color.fromRGBO(246, 245, 250, 1);
   static const Color disable = Color.fromRGBO(218, 218, 218, 1);
